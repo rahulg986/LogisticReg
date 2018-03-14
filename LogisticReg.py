415366f9b1832849fb746a0aa0473322acada2b1
Rahul Gupta <rahul@Rahuls-MBP.hsd1.co.comcast.net>
2018-03-13 22:53:49 -0600

    some fixes and addition like confusion matrix

--- LogisticReg.py
+++ LogisticReg.py
@@ -7,56 +7,71 @@ Created on Sun Mar 11 22:54:25 2018
 """
 #Import Libraries
 
-import numpy as np
+#import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+#from scipy import stats
+from sklearn.metrics import confusion_matrix
+import statsmodels.api as sm
 
 #%matplotlib inline
-ad_data = pd.read_csv('/Users/rahul/Desktop/Project/Logistic/data.csv')
+data = pd.read_csv('/Users/rahul/Desktop/Prog/LogisticReg/data.csv')
 #Check the head of ad_data
-ad_data.head(3)
+data.head()
 
-ad_data.info()
-
-ad_data.describe()
+data.info()
+        
+data.describe()
 
 #Exploratory Data Analysis
+data.hist(figsize=[10,10],color="teal")
 #Create a histogram of the Age
-sns.distplot(ad_data['Age'], bins=20)
+sns.distplot(data['Age'], bins=20)
 #Create a jointplot showing Area Income versus Age.
-sns.jointplot(ad_data['Area Income'], ad_data['Age'])
+sns.jointplot(data['Area Income'], data['Age'])
 #Create a jointplot showing the kde distributions of Daily Time spent on site vs. Age.
-sns.jointplot(ad_data['Age'], ad_data['Daily Time Spent on Site'], kind= 'kde')
-#sns.jointplot(ad_data['Daily Time Spent on Site'], ad_data['Daily Internet Usage'])
-sns.jointplot(ad_data['Daily Time Spent on Site'], ad_data['Daily Internet Usage'])
+sns.jointplot(data['Age'], data['Daily Time Spent on Site'], kind= 'kde')
+#sns.jointplot(data['Daily Time Spent on Site'], data['Daily Internet Usage'])
+sns.jointplot(data['Daily Time Spent on Site'], data['Daily Internet Usage'])
 #Create a pairplot with the hue defined by the 'Clicked on Ad' column feature.
-sns.pairplot(ad_data, hue='Clicked on Ad')
+sns.pairplot(data, hue='Clicked on Ad')
 
 
 #Logistic Regression
 
 #Split the data into training set and testing set using train_test_split
 
-ad_data.drop(['Ad Topic Line', 'City', 'Country', 'Timestamp'], axis=1, inplace=True)
-X = ad_data.drop(['Clicked on Ad'], axis = 1)
-y = ad_data['Clicked on Ad']
-
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=101)
-logmodel = LogisticRegression()
+data.drop(['Ad Topic Line', 'City', 'Country', 'Timestamp'], axis=1, inplace=True)
+X = data.drop(['Clicked on Ad'], axis = 1)
+print(X)
+Y = data['Clicked on Ad']
+print(Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3,random_state=0)
+model = LogisticRegression()
 
 #Train and fit a logistic regression model on the training set.
-logmodel.fit(X_train, y_train)
+model.fit(X_train, Y_train)
+
+#Model Stats
+#stats.chisqprob = lambda chisq, df: stats.chi2.sf(chisq, df)
+logitModel=sm.Logit(Y_train,X_train)
+result=logitModel.fit()
+print(result.summary())
 
 #Predictions and Evaluations
 
 #Now predict values for the testing data.
 
-predictions = logmodel.predict(X_test)
-predictions
+predictions = model.predict(X_test)
+print(predictions)
+
+# Making the Confusion Matrix 
+confMatrix = confusion_matrix(Y_test, predictions)
+confMatrix
 
 #Create a classification report for the model.
-print(classification_report(y_test, predictions))
+print(classification_report(Y_test, predictions))
